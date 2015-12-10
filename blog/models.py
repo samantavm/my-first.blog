b1c0ec65af669fbb88c74d6c32af35a824782128
svillanueva <svillanueva@lst.tfo.upm.es>
2015-12-10 15:11:40 +0100

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 from django.db import models
 from django.utils import timezone
+
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -14,21 +14,5 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-=======
-from django.db import models
-from django.utils import timezone
-
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
->>>>>>> 5a28a6ed229118d112d80595d00b57c6d85ab7d2
         return self.title
+
